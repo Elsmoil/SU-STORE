96900018b5303a76921636d6984a58f118ce756c
Elsmoil <alsmoilalrasta999@gmail.com>
2025-01-24 18:12:41 -0600

    fixing delete proudct conflict

--- backend/controllers/product.co.js
+++ backend/controllers/product.co.js
@@ -51,8 +51,8 @@ export const deleteProduct =  async (req, res) => {
         return res.status(404).json({success: false, message: 'Invalid product id'});
     }
     try {
-        await Product.findByIdAndRemove(id);
-        res.status(200).json({ success: true, message: "Product removed" });
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product Deleted" });
     } catch (error) {
         console.log("Error in deleting product", error.message);
         res.status(500).json({ success: false, message: "Server error" });
